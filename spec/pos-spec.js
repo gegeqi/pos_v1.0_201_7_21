e231c2ef4422cd44c549aeac590dffb8d4c037a6
gegeqi <ccczzz1015@163.com>
2016-07-23 08:28:59 +0800

    the first

--- spec/pos-spec.js
+++ spec/pos-spec.js
@@ -1,1 +1,365 @@
 'use strict';
+
+describe("formatTags",function(){
+    it("formatTags",function () {
+        let tags=[
+            'ITEM000002-2',
+            'ITEM000000',
+            'ITEM000005'
+        ];
+
+        let result=formatTags(tags);
+
+        let itemsTag = [
+            {
+                barcode:'ITEM000002',
+                amount:2
+            },
+            {
+                barcode:'ITEM000000',
+                amount:1
+            },
+            {
+                barcode:'ITEM000005',
+                amount:1
+            }
+        ];
+        expect(result).toEqual(itemsTag);
+    });
+});
+
+describe("mergeBarcodes",function () {
+    it("mergeBarces",function (){
+        let barcodes=[
+
+            {
+                barcode:"0001",
+                amount:1
+            },
+
+            {
+                barcode:"0002",
+                amount:1
+            },
+
+            {
+                barcode:"0001",
+                amount:2
+            }
+        ];
+
+        let result=mergeBarcodes(barcodes);
+
+        let items=[
+            {
+                barcode:"0001",
+                amount:3
+            },
+            {
+                barcode:"0002",
+                amount:1
+            }
+        ]
+        expect(result).toEqual(items);
+    });
+});
+
+ describe("changeType",function ( ) {
+
+    it("changeType",function () {
+        let input=[
+            {
+                type:"333",
+                barcodes:["001","002"]
+            },
+        ];
+        let text=changeType(input);
+        let result=[
+            {
+                type:"333",
+                barcode:"001"
+            },{
+                type:"333",
+                barcode:"002"
+            }
+        ];
+        expect(text).toEqual(result);
+
+    })
+});
+
+describe("addType",function (){
+    it("addType",function () {
+        let withType=[
+            {
+                barcode:"0001",
+                type:"333"
+            },
+            {
+                barcode:"0002"
+            }
+
+        ];
+        let allItems=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"000"
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"000"
+            }]
+        let result=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"000",
+            type:"333"
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"000",
+                type:"null"
+            }]
+
+        let text=addType(withType,allItems);
+        expect(text).toEqual(result);
+    })
+})
+
+describe("getCartItems",function ( ) {
+
+    it("getCartItems",function () {
+        let allTypeItems=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000"
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined"
+            }]
+        let allAmount=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            amount:2
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                amount:3
+            }]
+        let result=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3
+            }]
+        let text=getCartItems(allAmount,allTypeItems);
+        expect(text).toEqual(result);
+    })
+});
+
+describe("getPromotionSubtotal",function ( ) {
+
+    it("getPromotionSubtotal",function () {
+        let cartItems=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3
+            }]
+        let result=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2,
+            promotionSubtotal:6
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3,
+                promotionSubtotal:2
+            }]
+        let text=getPromotionSubtotal(cartItems);
+
+        expect(text).toEqual(result);
+    })
+});
+
+describe("calculateTotal",function ( ) {
+
+    it("calculateTotal",function () {
+        let input=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2,
+            promotionSubtotal:6
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3,
+                promotionSubtotal:2
+            }]
+        let result=8;
+        let text=calculateTotal(input);
+
+        expect(result).toEqual(result);
+    })
+});
+
+describe("getSubtotal",function ( ) {
+
+    it("getSubtotal",function () {
+        let input=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2,
+            promotionSubtotal:6,
+
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3,
+                promotionSubtotal:2,
+
+            }]
+        let result=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2,
+            promotionSubtotal:6,
+            subtotal:6
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3,
+                promotionSubtotal:2,
+                subtotal:3
+            }]
+        let text=calculateTotal(input);
+
+        expect(result).toEqual(result);
+    })
+});
+
+describe("calculateSaveMoney",function ( ) {
+
+    it("calculateSaveMoney",function () {
+        let input=[{
+            barcode:"0001",
+            name:"li",
+            price:3,
+            util:"个",
+            type:"000",
+            amount:2,
+            promotionSubtotal:6,
+            subtotal:6
+        },
+            {
+                barcode:"0002",
+                name:"wang",
+                price:1,
+                util:"个",
+                type:"undefined",
+                amount:3,
+                promotionSubtotal:2,
+                subtotal:3
+            }]
+        let result=1;
+        let text=calculateSaveMoney(input);
+
+        expect(text).toEqual(result);
+    })
+});
+
+describe("print", function () {
+    it("print", function () {
+
+        let subtotal = [{barcode:'ITEM000001',
+            name:'雪碧',
+            unit:'瓶',
+            price:3,
+            amount:1,
+            promotionSubtotal:2,
+            subtotal:3
+        }];
+        let total = 3;
+        let saveMoney = 1;
+
+        spyOn(console, 'log');
+
+        print(subtotal,total,saveMoney);
+
+        let result = "***没钱赚商店***收据\n" +
+            "商品:雪碧,单价:3元,件数:1,小计:2元\n" +
+            "************\n" +
+            "总计：3元,\n" +
+            "节省：1元";
+
+        expect(console.log).toHaveBeenCalledWith(result);
+    })
+})
+
+
--- src/pos.js
+++ src/pos.js
@@ -1,1 +1,217 @@
 'use strict';
+
+let tags = [
+    'ITEM000000-2',
+    'ITEM000002',
+    'ITEM000005'
+]
+function loadAllItems() {
+    return [
+        {
+            barcode: 'ITEM000000',
+            name: '可口可乐',
+            unit: '瓶',
+            price: 3.00
+        },
+        {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+        },
+        {
+            barcode: 'ITEM000002',
+            name: '苹果',
+            unit: '斤',
+            price: 5.50
+        },
+        {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+        },
+        {
+            barcode: 'ITEM000004',
+            name: '电池',
+            unit: '个',
+            price: 2.00
+        },
+        {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50
+        }
+    ];
+}
+function loadPromotions() {
+
+    return   [
+        {
+            type: 'BUY_TWO_GET_ONE_FREE',
+            barcodes: [
+                'ITEM000000',
+                'ITEM000001',
+                'ITEM000005'
+            ]
+        }
+    ];
+}
+function formatTags(tags){
+    return tags.map(function (items){
+        let info=items.split("-");
+        return {
+            barcode:info[0],
+            amount:parseFloat(info[1]) || 1
+        }
+    });
+}
+
+
+function mergeBarcodes(barcodes){
+    let allAmount=[];
+    for(let info of barcodes){
+        let exist=allAmount.find(function (items) {
+    return items.barcode===info.barcode;
+
+        })
+        if(exist){
+            exist.amount+=info.amount;
+        }else{
+            allAmount.push(Object.assign({},info,{amount:info.amount}));
+        }
+        }
+    return allAmount;
+}
+
+function changeType(allPromotions){
+    let withType=[];
+     for(let info of allPromotions){
+         for(let i=0;i<info.barcodes.length;i++){
+             if(info.type!=="null"){
+                 withType.push(Object.assign({},{type:info.type},{barcode:info.barcodes[i]}));
+             }else{
+                 info.type="null";
+             }
+         }
+         return withType;
+    }
+
+    function addType(withType,allItems){
+        let allTypeItems=[];
+        for(let info of withType){
+            for(let sofo of allItems){
+                if(info.barcode===sofo.barcode){
+                    allTypeItems.push(Object.assign({},sofo,{type:info.type}))
+                }else{
+                    info.type="null";
+                }
+            }
+        }
+    }
+
+    function getCartItems(allAmount,allTypeItems) {
+        let cartItems = [];
+        for (let i = 0; i < allAmount.length; i++) {
+            for (let items of allTypeItems) {
+                if (allAmount[i].barcode === items.barcode) {
+                    cartItems.push(Object.assign({}, items.barcode, {amount: allAmount[i].amount}))
+                }
+            }
+        }
+        return cartItems;
+    }}
+
+function addType(withType,allItems){
+    let allTypeItems=[];
+    for(let info of withType){
+        for(let sofo of allItems){
+            if(info.barcode===sofo.barcode){
+                allTypeItems.push(Object.assign({},sofo,{type:info.type}));
+            }else{
+                info.type="null";
+            }
+        }
+    }
+    return allTypeItems;
+}
+
+function getCartItems(allAmount,allTypeItems) {
+    let cartItems=[];
+    for (let info of allAmount){
+        for(let sofo of allTypeItems){
+            if(info.barcode===sofo.barcode){
+                cartItems.push(Object.assign({},sofo,{amount:info.amount}));
+            }
+        }
+    }
+    return cartItems;
+}
+
+function getPromotionSubtotal(cartItems){
+    let promotionSubtotal=[];
+    for(let info of cartItems){
+        if(info.type !== "null"){
+            let result= info.price * (info.amount-(parseInt(info.amount / 3)));
+            promotionSubtotal.push(Object.assign({},info,{promotionSubtotal:result}))
+        }
+    }
+    return promotionSubtotal;
+
+}
+
+function calculateTotal(promotionSubtotal) {
+    let total=0;
+    for(let info of promotionSubtotal){
+        total += info.promotionSubtotal;
+    }
+    return total;
+}
+
+function getSubtotal(promotionSubtotal){
+    let subtotal=[];
+    for(let info of promotionSubtotal){
+        let result=0;
+        result=info.price * info.amount;
+        subtotal.push(Object.assign({},info,{subtotal:result}))
+    }
+    return subtotal;
+}
+
+function calculateSaveMoney(subtotal) {
+    let saveMoney=0;
+    for(let info of subtotal){
+        saveMoney += (info.subtotal-info.promotionSubtotal);
+    }
+    return saveMoney;
+}
+
+function print( subtotal,total,saveMoney) {{
+    let receipt = "***没钱赚商店***收据" + "\n";
+    for (let i = 0; i < subtotal.length; i++) {
+        receipt += "商品:" + subtotal[i].name +",单价:"+subtotal[i].price+
+            "元"+",件数:"+subtotal[i].amount+",小计:"+subtotal[i].promotionSubtotal+"元"+"\n";
+    }
+    receipt+="************\n"+"总计："+total + "元,"+"\n"+"节省："+saveMoney + "元";
+    console.log(receipt);
+    return receipt;
+}}
+
+function printReceipt(tags) {
+    let allItems=loadAllItems();
+    let allPromotions=loadPromotions();
+    let barcodes=formatTags(tags);
+    let allAmount=mergeBarcodes(barcodes);
+    let withType=changeType(allPromotions);
+    let allTypeItems=addType(withType,allItems);
+    let cartItems=getCartItems(allAmount,allTypeItems);
+    let promotionSubtotal=getPromotionSubtotal(cartItems);
+    let total=calculateTotal(promotionSubtotal);
+    let subtotal=getSubtotal(promotionSubtotal);
+    let saveMoney=calculateSaveMoney(subtotal);
+    print(subtotal,total,saveMoney);
+}
+printReceipt(tags);
+
+
